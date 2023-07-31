f0da2475cdd8e15b9c59c496da8586bf741284a1
bongbongsam <125950622+bongbongsam@users.noreply.github.com>
2023-07-31 13:56:37 +0900

    Update prevent_copy.js

--- js/prevent_copy.js
+++ js/prevent_copy.js
@@ -33,6 +33,42 @@ function showAlert() {
    restoreIframeContent(); // Call the restoreIframeContent function when the exit button is clicked
      });
 }
+
+function showpageAlert() {
+  if (document.querySelector("#alertContainer .alert")) {
+    // 이미 경고 알림 창이 존재하는 경우에는 추가적으로 생성하지 않음
+    return;
+  }
+ // 경고 메시지를 보여주기 위해 div 요소를 생성
+ var alertDiv = document.createElement("div");
+ alertDiv.className = "alert alert-primary alert-dismissible fade show";
+ alertDiv.setAttribute("role", "alert");
+
+ // "strong" 태그를 생성하여 강조 텍스트를 추가
+ var strongTag = document.createElement("strong");
+ strongTag.textContent = "!Focus out!";
+ alertDiv.appendChild(strongTag);
+
+ // 경고 내용을 추가
+ var message = document.createTextNode("   페이지를 벗어났습니다. ");
+ alertDiv.appendChild(message);
+ 
+ // "button" 요소를 생성하여 경고 메시지 닫기 버튼을 추가
+ var closeButton = document.createElement("button");
+ closeButton.type = "button";
+ closeButton.className = "btn-close";
+ closeButton.setAttribute("data-bs-dismiss", "alert");
+ closeButton.setAttribute("aria-label", "Close");
+ alertDiv.appendChild(closeButton);
+
+ // 경고 메시지를 페이지에 삽입   
+ var alertContainer = document.getElementById("alertContainer");
+     alertContainer.appendChild(alertDiv);
+
+ closeButton.addEventListener("click", function () {
+   restoreIframeContent(); // Call the restoreIframeContent function when the exit button is clicked
+     });
+}
 // make the frame invisible
 function noframe() {
   var iframe = document.getElementById("myIframe");
@@ -42,6 +78,11 @@ function noframe() {
 function restoreIframeContent() {
   var iframe = document.getElementById("myIframe");
   iframe.src = "swiper.html";
+}
+
+function pageout() {
+  noframe();
+  showpageAlert();
 }
 
 
